c6e9df698448d6c4495e8142b0af5d39b3688bcc
MillerMagnus <millermagnus@outlook.com>
2024-02-20 22:03:21 -0300

    Aplicação base
    Bootstrap adicionado em document.js

---
+++ lampada-on-off/src/pages/index.js
@@ -0,0 +1,15 @@
+
+export default function Home() {
+
+  const Teste01 = () => {
+    console.log("chegou aqui")
+  }
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+       
+      </div>
+    </div>
+  );
+}
